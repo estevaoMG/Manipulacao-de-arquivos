23e15204b45ba8e098f3858f3490e221d9c0d35e
estevaoMG <estevao.gouveia@hotmail.com>
2024-07-09 23:36:11 -0300

    Libs Os e Shutil.

---
+++ 3_os_shutil.py
@@ -0,0 +1,14 @@
+import os
+import shutil
+
+
+# os.mkdir("novo-diretorio")
+
+# arquivo = open("novo.txt", "w")
+# arquivo.close()
+
+# os.rename("novo.txt", "alterado.txt")
+
+# os.remove("alterado.txt")
+
+shutil.move("novo.txt", "novo-diretorio")
